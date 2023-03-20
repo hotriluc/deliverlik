d2be908f1e8a53a3f157750cb030a5c6d6381e58
Luc Ho <hotriluc97@gmail.com>
2023-03-20 23:34:07 +0100

    feat: add cors and proxy

--- deliverlik-fe/src/App.tsx
+++ deliverlik-fe/src/App.tsx
@@ -1,9 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    axios.get("/api/v1/hi", {}).then((res) => console.log(res));
+  }, []);
 
   return (
     <div className="App">
@@ -15,7 +21,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React + TS</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -28,7 +34,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- deliverlik-fe/vite.config.ts
+++ deliverlik-fe/vite.config.ts
@@ -7,5 +7,31 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
+
+    cors: true,
+    // In production should replace localhost
+    proxy: {
+      "/api/v1": {
+        // because we run everything inside the container we should target by container's name
+        target: "http://api:3000",
+        changeOrigin: true,
+        secure: false,
+        configure: (proxy, _options) => {
+          proxy.on("error", (err, _req, _res) => {
+            console.log("proxy error", err);
+          });
+          proxy.on("proxyReq", (proxyReq, req, _res) => {
+            console.log("Sending Request to the Target:", req.method, req.url);
+          });
+          proxy.on("proxyRes", (proxyRes, req, _res) => {
+            console.log(
+              "Received Response from the Target:",
+              proxyRes.statusCode,
+              req.url
+            );
+          });
+        },
+      },
+    },
   },
 });
