1c31b13b39178f40f8154d802c274e7a43df50d2
Luc Ho <hotriluc97@gmail.com>
2023-03-28 21:53:24 +0200

    feat: add slideshow panel component

--- deliverlik-fe/src/components/slideshow-panel/SlideShowPanel.tsx
+++ deliverlik-fe/src/components/slideshow-panel/SlideShowPanel.tsx
@@ -1,7 +1,53 @@
 import React from "react";
+import styled from "styled-components";
+
+const Panel = styled.div`
+  margin-top: 5rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2rem;
+  background-color: ${(props) => props.theme.colors.primary};
+  padding: 2rem;
+  min-height: 30rem;
+`;
+
+const PanelRow = styled.div`
+  display: flex;
+  gap: 2rem;
+`;
+
+const PanelItem = styled.div`
+  /* min-width: 30rem; */
+  padding: 2rem;
+  background-color: ${(props) => props.theme.colors.background};
+  border-radius: ${(props) => props.theme.borderRadius};
+  box-shadow: ${(props) => props.theme.boxShadows.primaryBackground};
+
+  min-height: 13rem;
+  max-height: 13rem;
+`;
 
 const SlideShowPanel = () => {
-  return <div>SlideShowPanel</div>;
+  return (
+    <Panel>
+      <PanelRow>
+        <PanelItem style={{ width: "20rem" }}>Item1</PanelItem>
+        <PanelItem style={{ width: "20rem" }}>Item1</PanelItem>
+        <PanelItem style={{ width: "20rem" }}>Item1</PanelItem>
+        <PanelItem style={{ width: "20rem" }}>Item1</PanelItem>
+        <PanelItem style={{ width: "20rem" }}>Item1</PanelItem>
+        <PanelItem style={{ width: "20rem" }}>Item1</PanelItem>
+      </PanelRow>
+      <PanelRow>
+        <PanelItem style={{ width: "20rem" }}>Item1</PanelItem>
+        <PanelItem style={{ width: "20rem" }}>Item1</PanelItem>
+        <PanelItem style={{ width: "20rem" }}>Item1</PanelItem>
+        <PanelItem style={{ width: "20rem" }}>Item1</PanelItem>
+        <PanelItem style={{ width: "20rem" }}>Item1</PanelItem>
+      </PanelRow>
+    </Panel>
+  );
 };
 
 export default SlideShowPanel;
